8b496def403352524f8fc3f0107a4d994f6ddccf
erenakgunduz <coderenius@gmail.com>
2023-12-02 00:51:13 -0500

    Update GithubButton.story.tsx
    to demonstrate adding more stories

--- components/GithubButton/GithubButton.story.tsx
+++ components/GithubButton/GithubButton.story.tsx
@@ -2,3 +2,4 @@ import GithubButton from './GithubButton';
 
 export default { title: 'GitHub button', tags: ['autodocs'] };
 export const Default = () => <GithubButton>Login with GitHub</GithubButton>;
+export const Secondary = () => <GithubButton radius="lg">Anotha one</GithubButton>;
