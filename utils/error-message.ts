80998826ad21eee8141768f9d4f9f1e8f4134523
erenakgunduz <coderenius@gmail.com>
2023-12-05 23:25:11 -0500

    Create error-message.ts

---
+++ utils/error-message.ts
@@ -0,0 +1,16 @@
+// Helper function to extract the error message as a string no matter what
+export default function getErrorMessage(error: unknown): string {
+  let message: string;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === 'object' && 'message' in error) {
+    message = String(error.message);
+  } else if (typeof error === 'string') {
+    message = error;
+  } else {
+    message = 'Something went wrong';
+  }
+
+  return message;
+}
